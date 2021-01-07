2e06a9b6b7da8358394a41db26636222b4a44abe
Jack Donofrio <johndonofrio2021@students.sjcadets.org>
2021-01-06 22:07:16 -0500

    Improved customizability

--- alarm.py
+++ alarm.py
@@ -1,35 +1,36 @@
-# No-snooze Alarm Clock v2
+# NO SNOOZE Alarm Clock v2
 # Originally written on March 19, 2020
-# Updated on January 6, 2021 at 9:55 PM
+# Updated last on January 6, 2021 at 10:07 PM
 
 import RPi.GPIO as GPIO
 import time
 import datetime
 
+# Day Constants #
+MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY = range(7) # Do not edit
+
 #### ALARM CLOCK SETTINGS #####
-WEEKENDS_ON = False
-SATURDAY = 5 # <-- Just made this to avoid using magic numbers in my code.
-HOUR = 6
-MINUTES = 30
-##############################
+SLEEP_IN_DAYS = [SATURDAY, SUNDAY] # Days you don't want the alarm clock to go off
+HOUR = 6 # Hour you want to wake up in military time
+MINUTES = 30 # Min you want to wake up at
+###############################
 
 # GPIO SETUP
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 BUZZER_PIN = 23
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
+#
 
+# Execution loop
 while True:
     current_time_object = datetime.datetime.now()
-    current_day = current_time_object.weekday()
-    current_hour = current_time_object.hour
-    current_minutes = current_time_object.minute
-
-    if current_hour == HOUR and current_minutes == MINUTES and (WEEKENDS_ON or current_day < SATURDAY):
+    current_day, current_hour, current_minutes = current_time_object.weekday(), current_time_object.hour, current_time_object.minute
+    
+    if current_hour == HOUR and current_minutes == MINUTES and current_day not in SLEEP_IN_DAYS:
         for x in range(100):
             print('Wake up. NOW!')
             GPIO.output(BUZZER_PIN, GPIO.HIGH) # Beeeep 
             time.sleep(1)
             GPIO.output(BUZZER_PIN, GPIO.LOW)
             time.sleep(1)
-
