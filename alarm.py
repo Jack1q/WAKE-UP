af967fe336d1a4528e78bd5f41b2210e2f1e646b
Jack1q <johndonofrio2021@students.sjcadets.org>
2021-01-14 15:01:40 -0500

    Only beeps for 1 minute now instead of 200 seconds

--- alarm.py
+++ alarm.py
@@ -1,6 +1,6 @@
 # NO SNOOZE Alarm Clock v2
 # Originally written on March 19, 2020
-# Updated last on January 6, 2021 at 10:07 PM
+# Updated last on January 14, 2021 at 3:00 PM
 
 import RPi.GPIO as GPIO
 import time
@@ -11,16 +11,15 @@ MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY = range(7) # Do n
 
 #### ALARM CLOCK SETTINGS #####
 SLEEP_IN_DAYS = [SATURDAY, SUNDAY] # Days you don't want the alarm clock to go off
-HOUR = 6 # Hour you want to wake up in military time
+HOUR = 6 # Hour you want to wake up in 24hr military time. So for example, 6 AM = 6 and 6 PM = 18
 MINUTES = 30 # Min you want to wake up at
 ###############################
-
-# GPIO SETUP
+# GPIO SETUP #
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-BUZZER_PIN = 23
+GPIO.setmode(GPIO.BOARD)
+BUZZER_PIN = 11
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
-#
+################################
 
 # Execution loop
 while True:
@@ -28,9 +27,10 @@ while True:
     current_day, current_hour, current_minutes = current_time_object.weekday(), current_time_object.hour, current_time_object.minute
     
     if current_hour == HOUR and current_minutes == MINUTES and current_day not in SLEEP_IN_DAYS:
-        for x in range(100):
+        for x in range(30):
             print('Wake up. NOW!')
             GPIO.output(BUZZER_PIN, GPIO.HIGH) # Beeeep 
             time.sleep(1)
             GPIO.output(BUZZER_PIN, GPIO.LOW)
             time.sleep(1)
+            
