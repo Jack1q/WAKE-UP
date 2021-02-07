87a225545ccbb5f37afedfbd5d6558d0ad19c0f6
Jack Donofrio <johndonofrio2021@students.sjcadets.org>
2021-02-07 16:09:48 -0500

    Clarified some code

--- alarm.py
+++ alarm.py
@@ -1,6 +1,7 @@
-# NO SNOOZE Alarm Clock v2
+# NO SNOOZE Alarm Clock v3
+# LCD Alarm Clock with wake-up buzzer
 # Originally written on March 19, 2020
-# Updated last on January 14, 2021 at 3:00 PM
+# Updated last on February 7, 2021 at 4:07 PM
 
 import RPi.GPIO as GPIO
 import time
@@ -12,9 +13,9 @@ import threading
 MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY = range(7) # Do not edit
 
 #### ALARM CLOCK SETTINGS #####
-SLEEP_IN_DAYS = [SUNDAY] # Days you don't want the alarm clock to go off
-HOUR = 15 # Hour you want to wake up in 24hr military time. So for example, 6 AM = 6 and 6 PM = 18
-MINUTES = 58 # Min you want to wake up at
+SLEEP_IN_DAYS = [SATURDAY, SUNDAY] # Days you don't want the alarm clock to go off
+HOUR = 6 # Hour you want to wake up in 24hr military time. So for example, 6 AM = 6 and 6 PM = 18
+MINUTES = 30 # Min you want to wake up at
 ###############################
 # GPIO SETUP #
 GPIO.setwarnings(False)
@@ -24,17 +25,19 @@ GPIO.setup(BUZZER_PIN, GPIO.OUT)
 ################################
 my_lcd = lcd()
 
+# Thread function for continuously updating the LCD clock display while still checking to buzz
+# This also allows the clock to keep ticking while the buzzer is going off
+# I will also add something to allow users occasionally display different LCD messages
 def update_lcd():
     while True:
         my_lcd.lcd_display_string(time.strftime('%I:%M:%S %p'), 1)
         my_lcd.lcd_display_string(time.strftime('%a %b %d, 20%y'), 2)
 
 if __name__ == '__main__':
+    # Create LCD Thread
     lcd_thread = threading.Thread(target=update_lcd,args=(),daemon=True).start()        
     # Execution loop
     while True:
-        # Add something to occasionally display different LCD messages
-
         # Check if it's time to beep alarm.
         current_time_object = datetime.datetime.now()
         current_day, current_hour, current_minutes = current_time_object.weekday(), current_time_object.hour, current_time_object.minute
