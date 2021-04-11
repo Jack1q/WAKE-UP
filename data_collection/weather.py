88c9e1f20970d76c3ed2a7d35f5b7bcc8130e946
Jack1q <johndonofrio2021@students.sjcadets.org>
2021-04-11 15:03:36 -0400

    Improved code quality in some places, still more work to be done

--- data_collection/weather.py
+++ data_collection/weather.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 class Weather:
     """ Accesses local weather data for the clock to occasionally scroll through """
-
+    @staticmethod
     def get_client_ip_address():
         """ Retrieve's client's public IP address """
         return requests.get('https://api64.ipify.org/').text
-
+    @staticmethod
     def get_latlong_from_ip_address(ip_address):
         """ Gets latitude and longitude from IP address """
         location_data = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
         return f"{location_data['latitude']},{location_data['longitude']}"
-
+    @staticmethod
     def get_weather_data_from_latlong(latlong):
         """ Accesses local weather data from the National Weather Service's API """
         # weather_data = requests.get('https://www.metaweather.com/api/location/search/?lattlong={}')
@@ -25,7 +25,7 @@ class Weather:
         forecast_description = next_forecast[0]['shortForecast']
 
         return temperature + ' ' + forecast_description
-
+    @staticmethod
     def create_weather_db():
         """ Creates new Database for storing Forecast data """
         db = Database()
@@ -33,7 +33,7 @@ class Weather:
             columns = [
                 Column('Description','TEXT')
             ])
-
+    @staticmethod
     def get_db_forecast_value():
         """ 
         Retrieves most recently stored forecast value.
@@ -43,7 +43,7 @@ class Weather:
         """
         db = Database()
         return db.get_value_from_table('Forecasts', 'Description')[-1][0]
-    
+    @staticmethod
     def store_forecast_in_db(new_forecast, is_first_value):
         """ 
         Stores forecast data into database. Adds a new row if none exists, otherwise update
@@ -54,7 +54,7 @@ class Weather:
             db.add_to_table('Forecasts', [Column('Description','TEXT',new_forecast)])
         else:
             db.update_value('Forecasts', Column('Description','TEXT',Weather.get_db_forecast_value()), new_forecast)
-
+    @staticmethod
     def get_latest_forecast():
         """ Accesses latest local forecast data. Updates forecast every 10 minutes."""
         Weather.create_weather_db()
