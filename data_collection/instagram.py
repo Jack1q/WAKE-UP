6dba324cd2e1d5860daf092728cf09fb2e916e1a
Jack1q <johndonofrio2021@students.sjcadets.org>
2021-06-24 19:34:48 -0400

    removed some verbose code

--- data_collection/instagram.py
+++ data_collection/instagram.py
@@ -1,15 +1,11 @@
+""" Module for fetching your Instagram data """
 import requests
 
-class Instagram:
-    """ Class for fetching your Instagram data """
-    def __init__(self, username):
-        self.username = username
-    
-    def get_instagram_userdata(self):
-        """ Fetches publicly available IG user data """
-        return requests.get(f'https://www.instagram.com/{self.username}/?__a=1').json()
-    
-    def get_follower_count(self):
-        """ Returns follower count for given username """
-        user_data = self.get_instagram_userdata()
-        return "Insta: " + str(user_data['graphql']['user']['edge_followed_by']['count'])
+def get_instagram_userdata(username):
+    """ Fetches publicly available IG user data """
+    return requests.get(f'https://www.instagram.com/{username}/?__a=1').json()
+
+def get_follower_count(username):
+    """ Returns follower count for given username """
+    user_data = get_instagram_userdata(username)
+    return "Insta: " + str(user_data['graphql']['user']['edge_followed_by']['count'])
--- data_collection/mail.py
+++ data_collection/mail.py
@@ -1,25 +1,22 @@
+""" Access info from mail inbox """
 import imaplib
 
-class Mail:
-    """ Access info from mail inbox """
-    def __init__(self, address, password):
-        self.address = address
-        self.password = password
+def get_unread_mail_count(address, password):
+    """ 
+    Simply fetches the number of unread emails in an inbox. 
+    Mine is currently set to Gmail, but the imap settings can be changed
+    for other inboxes. 
 
-    def get_unread_mail_count(self):
-        """ 
-        Simply fetches the number of unread emails in an inbox. 
-        Mine is currently set to Gmail, but the imap settings can be changed
-        for other inboxes. 
+    Like weather, I should store this in a database and only update the value
+    every minute or two because it takes a bit of time to fetch.
+    """
+    try:
+        imap_object = imaplib.IMAP4_SSL('imap.gmail.com', '993')
+        imap_object.login(address, password)
+        imap_object.select()
+        number_unread = str(len(imap_object.search(None, 'UnSeen')[1][0].split())) 
+    except:
+        number_unread = '?'
+    return number_unread + ' unread'
 
-        Like weather, I should store this in a database and only update the value
-        every minute or two because it takes a bit of time to fetch.
-        """
-        try:
-            imap_object = imaplib.IMAP4_SSL('imap.gmail.com', '993')
-            imap_object.login(self.address, self.password)
-            imap_object.select()
-            number_unread = str(len(imap_object.search(None, 'UnSeen')[1][0].split())) 
-        except:
-            number_unread = '?'
-        return number_unread + ' unread'
+    
