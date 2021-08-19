b855f34402b64c7e493d17dbff789b8f5e16d25f
Jack Donofrio <johndonofrio2021@students.sjcadets.org>
2021-08-18 21:00:50 -0400

    Minor typo that went unnoticed in testing

--- alarm.py
+++ alarm.py
@@ -24,5 +24,5 @@ if __name__ == '__main__':
     start_processes(process_list)
     while True:
         if input():
-            termiante_processes(process_list)
+            terminate_processes(process_list)
             break
