c50622a423f2bcaa0c8d57681cf82674b231dfcc
Jack Donofrio <john.a.donofrio.25@dartmouth.edu>
2022-01-27 14:14:07 -0500

    created run script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#####################################################################
+# run.sh                                                            #
+# run this script to turn on the clock                              #
+# Later, this command will feature more args and customizations for #
+# running the clock. For now, this is just boilerplate code.        #
+#                                                                   #
+# Jack Donofrio                                                     #
+#####################################################################
+
+echo Starting up clock...
+python3 alarm.py
