897073ccd556e69b779583fc2c937f1dbc2972e2
Jack Donofrio <john.a.donofrio.25@dartmouth.edu>
2022-07-18 19:35:25 -0400

    updated run.sh to prettify logs

--- run.sh
+++ run.sh
@@ -9,3 +9,9 @@
 
 echo Starting up clock...
 python3 alarm.py
+
+# clean up logfile - sometimes the same error gets repeated many times in a row; this
+# makes it so consecutively repeating lines get condensed with a parenthesized count next to them
+if [[ -f alarm.log ]]; then
+  echo "$(uniq -c alarm.log | awk '{ if ($1 != "1") {$(NF + 1) = "("$1")"}; $1 = ""} sub(FS, "")')" > alarm.log
+fi
