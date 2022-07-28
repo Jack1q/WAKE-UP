01982f538f193b7fdac579c718fb04fc386ee8df
pyr0m4nc3r <johndonofrio2021@students.sjcadets.org>
2022-07-27 21:01:50 -0400

    added sleep-in-days

--- web.py
+++ web.py
@@ -13,9 +13,6 @@ app.config['SECRET_KEY'] = 'temp_key'
 
 class SettingsForm(FlaskForm):
     """ Fields for alarm settings """
-    # NOTE: when user submits, only save values
-    # if they are new ; do not overwite unchanged vals
-    # with blank data
 
     stock_ticker = StringField("Stock Ticker", validators=[Length(max=5)])
     custom_message = StringField("Custom Message", validators=[Length(max=16)])
@@ -24,10 +21,6 @@ class SettingsForm(FlaskForm):
     email_address = None
     email_password = None
     instagram_username = StringField("Instagram Username", validators=[Length(max=25)])
-
-    # Note - this does not work
-    # sleep_in_days = [BooleanField(label=day) for day in calendar.day_name]
-
 
     # countdown_year = IntegerField('Year')
     # countdown_month = IntegerField('Month')
@@ -42,6 +35,15 @@ class SettingsForm(FlaskForm):
         if alarm_minute == None or alarm_minute.data == None or alarm_minute.data < 0 or alarm_minute.data > 59:
             raise ValidationError("Minute must be between 0 and 59.")
 
+
+def add_sleep_in_attrs():
+    for e, day in enumerate(calendar.day_name):
+        setattr(SettingsForm, str(e), BooleanField(day[0:3]))
+
+def prettify_num(n):
+    if n < 10:
+        return '0' + str(n)
+    return str(n)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -64,27 +66,12 @@ def home():
         if form.alarm_minute.data != settings["ALARM_MINUTES"]:
             settings["ALARM_MINUTES"] = form.alarm_minute.data
 
-        # for e, day_form in enumerate(form.sleep_in_days):
-        #     if day_form.data and e not in settings["SLEEP_IN_DAYS"]:
-        #         settings["SLEEP_IN_DAYS"].append(e)
-        #     elif not day_form.data and e not in settings["SLEEP_IN_DAYS"]:
-        #         settings["SLEEP_IN_DAYS"].remove(e)
-
-
-        
-        # if form.sleep_in_monday.data and 0 not in settings["SLEEP_IN_DAYS"]:
-        #     settings["SLEEP_IN_DAYS"].append(0)
-        # elif not form.sleep_in_monday.data and 0 in settings["SLEEP_IN_DAYS"]:
-        #     settings["SLEEP_IN_DAYS"].remove(0)
-        # if form.sleep_in_tuesday.data and 1 not in settings["SLEEP_IN_DAYS"]:
-        #     settings["SLEEP_IN_DAYS"].append(1)
-        # elif not form.sleep_in_tuesday.data and 1 in settings["SLEEP_IN_DAYS"]:
-        #     settings["SLEEP_IN_DAYS"].remove(1)
-
-        # sleep_in_list = sorted([int(d) for d in form.sleep_in_days.data])
-        # saved_sleep_in_list = sorted(settings["SLEEP_IN_DAYS"])
-        # if sleep_in_list != saved_sleep_in_list:
-        #     settings["SLEEP_IN_DAYS"] = sleep_in_list
+        for e in range(len(calendar.day_name)):
+            sleep_in = getattr(form, str(e)).data
+            if sleep_in and e not in settings["SLEEP_IN_DAYS"]:
+                settings["SLEEP_IN_DAYS"].append(e)
+            elif not sleep_in and e in settings["SLEEP_IN_DAYS"]:
+                settings["SLEEP_IN_DAYS"].remove(e)
         
         flash('settings updated', 'success')
         config.update_settings_file(settings)
@@ -94,12 +81,12 @@ def home():
         form.custom_message.data = settings["CUSTOM_MESSAGE"]
         form.stock_ticker.data = settings["STOCK_TICKER"]
         form.instagram_username.data = settings["INSTAGRAM_USERNAME"]
-        form.alarm_hour.data = settings["ALARM_HOUR"]
-        form.alarm_minute.data = settings["ALARM_MINUTES"]
+        form.alarm_hour.data = prettify_num(settings["ALARM_HOUR"])
+        form.alarm_minute.data = prettify_num(settings["ALARM_MINUTES"])
         # form.sleep_in_monday.data = 0 in settings["SLEEP_IN_DAYS"]
-        # for i in range(len(form.sleep_in_days)):
-        #     form.sleep_in_days[i].data = i in settings["SLEEP_IN_DAYS"]
-        # form.sleep_in_days.data = settings["SLEEP_IN_DAYS"]
+        for i in range(len(calendar.day_name)):
+            getattr(form, str(i)).data = i in settings["SLEEP_IN_DAYS"]
+        getattr(form, str(i)).data = settings["SLEEP_IN_DAYS"]
 
     return render_template('index.html', form=form)
 
@@ -109,4 +96,5 @@ def about():
     return "about"
 
 if __name__ == '__main__':
+    add_sleep_in_attrs()
     app.run(debug=True)
